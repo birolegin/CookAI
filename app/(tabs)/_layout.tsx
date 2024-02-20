2067ebcc35e85f530ebcc531e6e81dcdf14aa15d
Birol Egin <birolegin03@gmail.com>
2024-02-20 17:17:07 +0300

    feat : Restore WebID

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,44 @@
-import React from 'react'
-import { Tabs } from 'expo-router'
-import theme from '../../custom-theme.json'
+import React from "react";
+import { Tabs } from "expo-router";
+import theme from "../../custom-theme.json";
 
 const Page = () => {
-    return (
-        <Tabs>
-            <Tabs.Screen
-                name='home'
-                options={{
-                    title: "Ana sayfa",
-                    tabBarLabel: "Ana sayfa",
-                    tabBarStyle: {
-                        backgroundColor: theme['color-basic-400'],
-                    },
-                    headerStyle: {
-                        backgroundColor: theme['color-basic-400'],
-                    },
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                }}
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Ana sayfa",
+          tabBarLabel: "Ana sayfa",
+          tabBarStyle: {
+            backgroundColor: theme["color-primary-100"],
+          },
+          headerStyle: {
+            backgroundColor: theme["color-primary-100"],
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profil",
+          tabBarLabel: "Profil",
+          tabBarStyle: {
+            backgroundColor: theme["color-basic-transparent-600"],
+          },
+          headerStyle: {
+            backgroundColor: theme["color-basic-transparent-600"],
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
+    </Tabs>
+  );
+};
 
-            />
-            <Tabs.Screen
-                name='profile'
-                options={{
-                    title: "Profil",
-                    tabBarLabel: "Profil",
-                    tabBarStyle: {
-                        backgroundColor: theme['color-basic-transparent-600'],
-                    },
-                    headerStyle: {
-                        backgroundColor: theme['color-basic-transparent-600'],
-                    },
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                }}
-            />
-        </Tabs>
-    )
-}
-
-export default Page
+export default Page;
--- app/index.tsx
+++ app/index.tsx
@@ -1,97 +1,121 @@
-import { View, StyleSheet, TextInput, Alert, KeyboardAvoidingView } from 'react-native'
-import React, { useContext, useEffect, useState } from 'react'
-import { router, useRouter } from 'expo-router'
-import { User, onAuthStateChanged, signInWithEmailAndPassword, signInWithCredential, GoogleAuthProvider } from 'firebase/auth'
-import { FIREBASE_AUTH } from '../FirebaseConfig'
-import { GoogleSignin, GoogleSigninButton } from '@react-native-google-signin/google-signin'
-import { Layout, Text, Input, Button, Card } from '@ui-kitten/components';
-
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Alert,
+  KeyboardAvoidingView,
+} from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { router, useRouter } from "expo-router";
+import {
+  User,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithCredential,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { FIREBASE_AUTH } from "../FirebaseConfig";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
+import { Layout, Text, Input, Button, Card } from "@ui-kitten/components";
 
 const Page = () => {
-    const route = useRouter()
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [loading, setLoading] = useState(false)
-    const auth = FIREBASE_AUTH
+  const route = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH;
 
-    useEffect(() => {
-        GoogleSignin.configure({
-            "webClientId": "1044771271544-4ca2qoibgu9tu3hjqlppj73bush8k41h.apps.googleusercontent.com",
-            offlineAccess: true
-        })
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        "892809679715-fenqeqgeco7297fiaphpejpon1j16s0a.apps.googleusercontent.com",
+      offlineAccess: true,
+    });
 
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                route.replace('/home');
-            }
-        });
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        route.replace("/home");
+      }
+    });
 
-        return () => unsubscribe();
-    }, []);
+    return () => unsubscribe();
+  }, []);
 
-
-    const signIn = async () => {
-        setLoading(true);
-        try {
-            const response = signInWithEmailAndPassword(auth, email, password);
-            console.log((await response).user);
-            Alert.alert("Hoşgeldin", "Giriş başarılı!")
-            route.replace('/home');
-        } catch (error: any) {
-            console.log(error);
-            Alert.alert("Hata", "Giriş yapılamadı: " + error.message)
-        } finally {
-            setLoading(false);
-        }
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = signInWithEmailAndPassword(auth, email, password);
+      console.log((await response).user);
+      Alert.alert("Hoşgeldin", "Giriş başarılı!");
+      route.replace("/home");
+    } catch (error: any) {
+      console.log(error);
+      Alert.alert("Hata", "Giriş yapılamadı: " + error.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const signInGoogle = async () => {
-        try {
-            await GoogleSignin.hasPlayServices();
-            const googleUser: any = await GoogleSignin.signIn();
-            const googleCredential = GoogleAuthProvider.credential(googleUser.idToken);
-            const firebaseUser = await signInWithCredential(auth, googleCredential);
-            console.log(firebaseUser);
-            route.replace('/home');
-        } catch (error: any) {
-            console.error(error);
-        }
+  const signInGoogle = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const googleUser: any = await GoogleSignin.signIn();
+      const googleCredential = GoogleAuthProvider.credential(
+        googleUser.idToken
+      );
+      const firebaseUser = await signInWithCredential(auth, googleCredential);
+      console.log(firebaseUser);
+      route.replace("/home");
+    } catch (error: any) {
+      console.error(error);
     }
+  };
 
-    return (
-        <Layout style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
-            <KeyboardAvoidingView behavior='padding'>
-                <Text category='h1'>CookAI</Text>
+  return (
+    <Layout style={{ flex: 1, justifyContent: "center", padding: 20 }}>
+      <KeyboardAvoidingView behavior="padding">
+        <Text category="h1">CookAI</Text>
 
-                <Input
-                    value={email}
-                    placeholder='E-posta adresi'
-                    autoCapitalize='none'
-                    onChangeText={(text) => setEmail(text)}
-                    style={{ marginTop: 16 }}
-                />
+        <Input
+          value={email}
+          placeholder="E-posta adresi"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+          style={{ marginTop: 16 }}
+        />
 
-                <Input
-                    value={password}
-                    placeholder='Şifre'
-                    autoCapitalize='none'
-                    secureTextEntry={true}
-                    onChangeText={(text) => setPassword(text)}
-                    style={{ marginTop: 16 }}
-                />
+        <Input
+          value={password}
+          placeholder="Şifre"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          style={{ marginTop: 16 }}
+        />
 
-                <GoogleSigninButton size={GoogleSigninButton.Size.Wide} color={GoogleSigninButton.Color.Dark} onPress={signInGoogle} style={{ marginTop: 16 }} />
+        <GoogleSigninButton
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={signInGoogle}
+          style={{ marginTop: 16 }}
+        />
 
-                <Button onPress={signIn} style={{ marginTop: 16 }}>
-                    Giriş yap
-                </Button>
+        <Button onPress={signIn} style={{ marginTop: 16 }}>
+          Giriş yap
+        </Button>
 
-                <Button onPress={() => route.replace("/register")} style={{ marginTop: 16 }}>
-                    Hesabın yok mu? Kayıt ol
-                </Button>
-            </KeyboardAvoidingView>
-        </Layout>
-    )
-}
+        <Button
+          onPress={() => route.replace("/register")}
+          style={{ marginTop: 16 }}
+        >
+          Hesabın yok mu? Kayıt ol
+        </Button>
+      </KeyboardAvoidingView>
+    </Layout>
+  );
+};
 
-export default Page
+export default Page;
